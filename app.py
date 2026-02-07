06ff61936edc047c722109508af82a47519e5bb4
Abhinand. p .sreenivasan <abhinandpsreenivasan1@gmail.com>
2026-02-07 10:33:37 +0530

    Update imports to use langchain_community modules

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import streamlit as st
-import os
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
+from langchain_core.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
 
 #--------------------------
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
