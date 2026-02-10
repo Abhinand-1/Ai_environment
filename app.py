2182ed7263dd538b1af72297ce3f7fc9289def46
Abhinand. p .sreenivasan <abhinandpsreenivasan1@gmail.com>
2026-02-10 10:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
